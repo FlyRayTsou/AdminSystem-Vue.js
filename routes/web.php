347ce0f14cb2ae978cc39d68524f4a2471d569dd
Ray Tsou <ray.tsou@ilong-termcare.com>
2020-01-09 15:41:58 +0800

    Feature : pass parameter by url

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/member/info/{name}', function($name) {
+	return view("member/intro", [
+		"name" => $name
+	]);
+});
