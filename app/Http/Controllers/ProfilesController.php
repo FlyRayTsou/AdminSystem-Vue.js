413798426d3f420697e98684132a03c10e1f6067
Ray <ray.tsou@ilong-termcare.com>
2020-02-26 17:45:22 +0900

    Feature : 1.add user and profile simultaneously 2.add new column image on profile table 3.adjust web route 4.auth about edit profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -14,17 +15,34 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
+    	$this->authorize('update', $user->profile);
     	return view('profiles.edit', compact('user'));
     }
 
-    public function update()
+    public function update(User $user)
     {
+    	$this->authorize('update', $user->profile);
+
     	$data = request()->validate([
     		'title' => 'required',
     		'description' => 'required',
-    		'url' => '',
+    		'url' => 'url',
     		'image' => ''
     	]);
-    	dd($data);
+
+    	if(request('image')) {
+
+	        $imagePath = request('image')->store('profile', 'public');
+
+	        $image = image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+	        $image->save();
+    	}
+
+    	auth()->user()->profile->update(array_merge(
+    		$data,
+    		['image' => $imagePath]
+    	));
+
+    	return redirect("/profile/{$user->id}");
     }
 }
